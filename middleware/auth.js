7796f151a753802721218255e209e1471cadb103
Methre Amarnath <methreamarnath@gmail.com>
2025-07-19 22:43:01 +0530

    the auth middleware was built

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).send({ error: "Authentication required" });
+    }
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is not valid",
+      });
+    }
+
+    if (!user.isActive) {
+      return res.status(401).json({
+        success: false,
+        message: "Account is deactivated",
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Token is not valid",
+    });
+  }
+};
+
+module.exports = auth;
